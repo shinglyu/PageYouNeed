ce9fdd4a585dce72f550b4df324b3997a982fda3
Shing Lyu <shing.lyu@gmail.com>
2018-01-06 00:33:11 +0100

    Added the symmetric module and test

---
+++ symmetric_matrix.js
@@ -0,0 +1,33 @@
+class SymmetricMatrix {
+  constructor() {
+    this.matrix = {}
+  }
+  update(key1, key2, value) {
+    var [small_key, large_key] = key1 < key2 ? [key1, key2] : [key2, key1]
+    if (! (small_key in this.matrix)) {
+      this.matrix[small_key] = {}
+    }
+    if (! (large_key in this.matrix)) {
+      this.matrix[large_key] = {}
+    }
+    this.matrix[small_key][large_key] = value 
+  }
+  get(key1, key2){
+    var [small_key, large_key] = key1 < key2 ? [key1, key2] : [key2, key1]
+    if (! (small_key in this.matrix) || ! (large_key in this.matrix[small_key])) {
+      return 0
+    }
+    else {
+      return this.matrix[small_key][large_key]
+    }
+  }
+  get_row(key){
+    var row = this.matrix[key]; // incomplete set of values, this might speedup the for loop
+    for (var other_key of Object.keys(this.matrix)) {
+      if (! (other_key in row) && other_key != key) {
+        row[other_key] = this.get(key, other_key)
+      }
+    }
+    return row;
+  }
+}
---
+++ test.symmetric_matrix.js
@@ -0,0 +1,25 @@
+describe('Symmetric matrix', function() {
+  it('can be inserted', function() {
+    var matrix = new SymmetricMatrix()
+    matrix.update("A", "B", 5)
+    expect(matrix.get("A", "B")).to.be(5);
+  });
+  it('is symmetric', function() {
+    var matrix = new SymmetricMatrix()
+    matrix.update("A", "B", 5)
+    expect(matrix.get("B", "A")).to.be(5);
+  });
+  it('can get a row', function() {
+    var matrix = new SymmetricMatrix()
+    matrix.update("A", "B", 5)
+    matrix.update("B", "C", 10)
+    expect(matrix.get_row("B")).to.eql({"A": 5, "C": 10});
+  });
+  it('can handle sparse data', function() {
+    var matrix = new SymmetricMatrix()
+    matrix.update("A", "B", 5)
+    matrix.update("B", "C", 10)
+    matrix.update("A", "D", 15)
+    expect(matrix.get_row("B")).to.eql({"A": 5, "C": 10, "D": 0});
+  });
+});
