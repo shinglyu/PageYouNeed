c904c39928587b6b9a1b9ad0970c3fcf3e3e2dbe
Shing Lyu <shing.lyu@gmail.com>
2018-01-10 21:44:40 +0100

    Added http to https filter

--- filter.js
+++ filter.js
@@ -1,9 +1,25 @@
 function filter_noisy_urls(histories) {
   histories = filter_google_search(histories);
+  histories = filter_http_to_https(histories);
   return histories
 
 }
 
 function filter_google_search(histories) {
   return histories.filter(history => !history.url.match(/^http(s?):\/\/www.google.\w*\/search/))
+}
+
+function filter_http_to_https(histories) {
+  var seen = new Set();
+  for (var history of histories) {
+    var url = history.url;
+    if (url.startsWith("http://") && seen.has(url.replace("http", "https"))) {
+      continue; // Skip
+    }
+    else if (url.startsWith("https://") && seen.has(url.replace("https", "http"))) {
+      seen.delete(url.replace("https", "http"));
+    }
+    seen.add(url);
+  }
+  return histories.filter(history => seen.has(history.url));
 }
--- test.filter.js
+++ test.filter.js
@@ -11,4 +11,18 @@ describe('Filtering', function() {
     var result = filter_google_search(input)
     expect(result).to.eql(expected);
   });
+  it('can filter out http to https redirection', function() {
+    var input = [
+      {"url":"https://www.test.com" }, 
+      {"url":"http://www.test.com" }, 
+      {"url":"http://www.test2.com" }, 
+      {"url":"https://www.test2.com" }, 
+    ]
+    var expected = [
+      {"url":"https://www.test.com"},
+      {"url":"https://www.test2.com"},
+    ]
+    var result = filter_http_to_https(input)
+    expect(result).to.eql(expected);
+  });
 });
