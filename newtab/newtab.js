ee1172c05aeeca2961ee6ed223c88b6972bd1d63
Shing Lyu <shing.lyu@booking.com>
2018-01-11 15:20:49 +0100

    Added quick search

--- newtab/newtab.js
+++ newtab/newtab.js
@@ -5,7 +5,10 @@ chrome.tabs.query({}, function(tabs){
   console.log("before send")
   chrome.runtime.sendMessage({method: "get_suggestions", query: query}, function(response) {
     var ul = document.getElementById("suggestions_list");
+    var lines = 0;
+    const max_lines = 200;
     for (var suggestion of response.suggestions) {
+      if (lines > max_lines) { break; }
       var li = document.createElement("li");
       var title = document.createElement("b");
       var link = document.createElement("a");
@@ -16,7 +19,29 @@ chrome.tabs.query({}, function(tabs){
       li.appendChild(document.createTextNode("\u00A0"));
       li.appendChild(link);
       ul.appendChild(li);
+
+      lines += 1;
     }
   });
 })
       
+var g_debouce_timer = undefined;
+const debounce_time = 500;
+
+document.getElementById("search").addEventListener("input", function(event){
+  var query = event.target.value;
+  window.clearTimeout(g_debouce_timer);
+  g_debouce_timer = window.setTimeout(function(){
+    console.log("Search")
+    var lis = document.getElementsByTagName("li");
+    for (var li of lis) {
+      if (!li.textContent.toLowerCase().includes(query.toLowerCase())) {
+        li.classList.add("hidden");
+      }
+      else {
+        li.classList.remove("hidden");
+      }
+    }
+  }, debounce_time);
+});
+
